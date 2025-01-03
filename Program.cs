31e4dc1812f54589ea8cc520a1e7b90a4a26a780
Risto Ikonen <ristoikonen@gmail.com>
2025-01-03 17:41:53 +1100

    AI App using Phi3 models.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection.Emit;
+using System.Reflection;
+using Microsoft.ML.OnnxRuntimeGenAI;
+
+
+// Console.WriteLine("Hello, World!");
+// folder location of the ONNX model file
+// phi-3-directml-int4-awq-block-128
+// cpu-int4-rtn-block-32-acc-level-4
+var modelPath = @"C:\tmp\models\phi-3-directml-int4-awq-block-128";
+var model = new Model(modelPath);
+var tokenizer = new Tokenizer(model);
+
+var systemPrompt = "You are an AI assistant that helps people find information. Answer questions using a direct style. Do not share more information that the requested by the users.";
+
+// chat start
+Console.WriteLine(@"Ask your question. Type an empty string to Exit.");
+
+// chat loop
+while (true)
+{
+    // Get user question
+    Console.WriteLine();
+    Console.Write(@"Q: ");
+    var userQ = Console.ReadLine();
+    if (string.IsNullOrEmpty(userQ))
+    {
+        break;
+    }
+
+    // show phi3 response
+    Console.Write("Phi3: ");
+    //var fullPrompt = $"<|system|>{systemPrompt}<|end|><|user|>{userQ}<|end|><|assistant|>";
+    var fullPrompt = $"<|system|>chemistry{systemPrompt}<|end|><|user|>{userQ}<|end|><|assistant|>";
+
+    var tokens = tokenizer.Encode(fullPrompt);
+
+    var generatorParams = new GeneratorParams(model);
+    generatorParams.SetSearchOption("max_length", 2048);
+    generatorParams.SetSearchOption("past_present_share_buffer", false);
+    generatorParams.SetInputSequences(tokens);
+
+    var generator = new Generator(model, generatorParams);
+    while (!generator.IsDone())
+    {
+        generator.ComputeLogits();
+        generator.GenerateNextToken();
+        //        var e    = generator.SetActiveAdapter(GetOutput("e");
+        var outputTokens = generator.GetSequence(0);
+        var newToken = outputTokens.Slice(outputTokens.Length - 1, 1);
+        var output = tokenizer.Decode(newToken);
+        Console.Write(output);
+    }
+    Console.WriteLine();
+}
