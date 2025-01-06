d38ca4fe48e005c01054d3e39c9dac686e26c8fa
Risto Ikonen <ristoikonen@gmail.com>
2025-01-06 19:39:08 +1100

    Add image model usage

--- Program.cs
+++ Program.cs
@@ -1,56 +1,307 @@
 ﻿using System;
 using System.Reflection.Emit;
 using System.Reflection;
+using System.ComponentModel;
 using Microsoft.ML.OnnxRuntimeGenAI;
+//using Microsoft.Extensions.Configuration.CommandLine;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
 
 
-// Console.WriteLine("Hello, World!");
-// folder location of the ONNX model file
-// phi-3-directml-int4-awq-block-128
-// cpu-int4-rtn-block-32-acc-level-4
-var modelPath = @"C:\tmp\models\phi-3-directml-int4-awq-block-128";
-var model = new Model(modelPath);
-var tokenizer = new Tokenizer(model);
-
-var systemPrompt = "You are an AI assistant that helps people find information. Answer questions using a direct style. Do not share more information that the requested by the users.";
-
-// chat start
-Console.WriteLine(@"Ask your question. Type an empty string to Exit.");
-
-// chat loop
-while (true)
+namespace Phi3
 {
-    // Get user question
-    Console.WriteLine();
-    Console.Write(@"Q: ");
-    var userQ = Console.ReadLine();
-    if (string.IsNullOrEmpty(userQ))
+
+    class Phil3
     {
-        break;
-    }
 
-    // show phi3 response
-    Console.Write("Phi3: ");
-    //var fullPrompt = $"<|system|>{systemPrompt}<|end|><|user|>{userQ}<|end|><|assistant|>";
-    var fullPrompt = $"<|system|>chemistry{systemPrompt}<|end|><|user|>{userQ}<|end|><|assistant|>";
+        // CMD: phi3 "C:\tmp\models\phi-3-directml-int4-awq-block-128" chat
 
-    var tokens = tokenizer.Encode(fullPrompt);
+        public const string DEFAULT_MODEL_PATH = @"C:\tmp\models\phi-3-directml-int4-awq-block-128";
 
-    var generatorParams = new GeneratorParams(model);
-    generatorParams.SetSearchOption("max_length", 2048);
-    generatorParams.SetSearchOption("past_present_share_buffer", false);
-    generatorParams.SetInputSequences(tokens);
+        static async Task<int> Main(string[] args)
+        {
+            if (args.Length < 1)
+                return await RunApp("", "chat");
+            if (args.Length == 1)
+                return await RunApp(args[0], "chat");
+            else
+                return await RunApp(args[0], args[1]);
 
-    var generator = new Generator(model, generatorParams);
-    while (!generator.IsDone())
+            /*
+             
+            while (i < args.Length)
+{
+    var arg = args[i];
+    if (arg == "--non-interactive")
     {
-        generator.ComputeLogits();
-        generator.GenerateNextToken();
-        //        var e    = generator.SetActiveAdapter(GetOutput("e");
-        var outputTokens = generator.GetSequence(0);
-        var newToken = outputTokens.Slice(outputTokens.Length - 1, 1);
-        var output = tokenizer.Decode(newToken);
-        Console.Write(output);
+        interactive = false;
     }
-    Console.WriteLine();
+    else if (arg == "-m")
+    {
+        if (i + 1 < args.Length)
+        {
+            modelPath = Path.Combine(args[i+1]);
+        }
+    }
+    else if (arg == "-e")
+    {
+        if (i + 1 < args.Length)
+        {
+            executionProvider = Path.Combine(args[i+1]);
+        }
+    }
+    i++;
 }
+              
+              */
+
+
+
+        }
+
+        static async Task<int> RunApp(string path, string? usage)
+        {
+            var APP_MODELUSE = Phi3.MODELUSE.IMAGE;
+            if (usage is not null && usage.ToLower().StartsWith("image") || usage is not null && usage.ToLower().StartsWith("img"))
+                APP_MODELUSE = MODELUSE.IMAGE;
+
+            var general_msg = "Phi-3 Mini open model is made by Microsoft. It's 3.8 billion parameter model that's trained on synthetic data and filtered publicly available websites. It's instruction-tuned, meaning it's trained to follow different types of instructions. Due to it's size it is weak on Factual Knowledge and does not shine with languages. Then agin it's Reasoning and math is on par with GPT 3.5 Turbo models";
+
+            var fileLocationsForImageApp = new FileLocations
+            {
+                ModelDirectory = @"C:\tmp\models\Phi-3.5-vision-instruct", //Phi-3-vision-128k-instruct-onnx-cpu\cpu-int4-rtn-block-32-acc-level-4",
+                ModelName = "cpu-int4-rtn-block-32-acc-level-4",
+                Image1_Dir = @"C:\tmp\images\BayRoad.png",
+                Image2_Dir = @"C:\tmp\images\BeanStrip3.png",
+                ModelUsage = "Uses CPU. Does Optical Character Recognition (OCR), Image Captioning, Table Parsing, and Reading Comprehension on Scanned Documents. Limited by its size store too much factual knowledge. Balances latency vs. accuracy. Model with acc-level-4 has better performance with a minor trade-off in accuracy."
+                // https://techcommunity.microsoft.com/blog/azure-ai-services-blog/phi-3-vision-%E2%80%93-catalyzing-multimodal-innovation/4170251
+            };
+
+            var fileLocationsForChatApp = new FileLocations
+            {
+                ModelDirectory = @"C:\tmp\models\phi-3-directml-int4-awq-block-128",
+                ModelName = "phi-3-directml-int4-awq-block-128",
+                ModelUsage = "Uses CPU. Lightweight, focus on very high-quality, reasoning dense data, open model built upon datasets used for Phi-2 - synthetic data and filtered websites - with a focus on very high-quality, reasoning dense data. "
+            };
+
+            if (APP_MODELUSE == MODELUSE.CHAT)
+            {
+                // folder location of the ONNX model file
+                // phi-3-directml-int4-awq-block-128
+                // cpu-int4-rtn-block-32-acc-level-4
+                var modelPath = @"C:\tmp\models\phi-3-directml-int4-awq-block-128";
+                var model = new Model(modelPath);
+                var tokenizer = new Tokenizer(model);
+
+                Console.WriteLine(fileLocationsForChatApp.ToString());
+
+                var systemPrompt = "You are an AI assistant that helps people find information. Answer questions using a direct style. Do not share more information that the requested by the users.";
+
+                // chat start
+                Console.WriteLine(@"Ask your question. Type an empty string to Exit.");
+
+                // chat loop
+                while (true)
+                {
+                    // Get user question
+                    Console.WriteLine();
+                    Console.Write(@"Q: ");
+                    var userQ = Console.ReadLine();
+                    if (string.IsNullOrEmpty(userQ))
+                    {
+                        break;
+                    }
+
+                    // show phi3 response
+                    Console.Write("Phi3: ");
+                    //var fullPrompt = $"<|system|>{systemPrompt}<|end|><|user|>{userQ}<|end|><|assistant|>";
+                    var fullPrompt = $"<|system|>chemistry{systemPrompt}<|end|><|user|>{userQ}<|end|><|assistant|>";
+
+                    var tokens = tokenizer.Encode(fullPrompt);
+
+                    var generatorParams = new GeneratorParams(model);
+                    generatorParams.SetSearchOption("max_length", 2048);
+                    generatorParams.SetSearchOption("past_present_share_buffer", false);
+                    generatorParams.SetInputSequences(tokens);
+
+                    var generator = new Generator(model, generatorParams);
+                    while (!generator.IsDone())
+                    {
+                        generator.ComputeLogits();
+                        generator.GenerateNextToken();
+                        //        var e    = generator.SetActiveAdapter(GetOutput("e");
+                        var outputTokens = generator.GetSequence(0);
+                        var newToken = outputTokens.Slice(outputTokens.Length - 1, 1);
+                        var output = tokenizer.Decode(newToken);
+                        Console.Write(output);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            if (APP_MODELUSE == MODELUSE.IMAGE)
+            {
+                // path for model and images
+                var modelPath = fileLocationsForImageApp.ModelDirectory;
+                //@"c:\tmp\models\Phi-3-vision-128k-instruct-onnx-cpu\cpu-int4-rtn-block-32-acc-level-4";
+
+                var foggyDayImagePath = fileLocationsForImageApp.Image1_Dir; // Path.Combine(Directory.GetCurrentDirectory(), "imgs", "foggyday.png");
+                var petsMusicImagePath = fileLocationsForImageApp.Image2_Dir; // Path.Combine(Directory.GetCurrentDirectory(), "imgs", "petsmusic.png");
+                var bothpaths = new string[] { foggyDayImagePath, petsMusicImagePath };
+                var img = Images.Load(bothpaths);
+
+                // define prompts
+                var systemPrompt = "You are an AI assistant that helps people find information. Answer questions using a direct style. Do not share more information that the requested by the users.";
+                string userPrompt = "Describe the image, and return the string 'STOP' at the end.";
+                var fullPrompt = $"<|system|>{systemPrompt}<|end|><|user|><|image_1|>{userPrompt}<|end|><|assistant|>";
+
+                // load model and create processor
+                using Model model = new Model(modelPath);
+                using MultiModalProcessor processor = new MultiModalProcessor(model);
+                using var tokenizerStream = processor.CreateStream();
+
+                // create the input tensor with the prompt and image
+                Console.WriteLine("Full Prompt: " + fullPrompt);
+                Console.WriteLine("Start processing image and prompt ...");
+                var inputTensors = processor.ProcessImages(fullPrompt, img);
+                using GeneratorParams generatorParams = new GeneratorParams(model);
+                generatorParams.SetSearchOption("max_length", 3072);
+                generatorParams.SetInputs(inputTensors);
+
+                // generate response
+                Console.WriteLine("Generating response ...");
+                using var generator = new Generator(model, generatorParams);
+                while (!generator.IsDone())
+                {
+                    generator.ComputeLogits();
+                    generator.GenerateNextToken();
+                    var seq = generator.GetSequence(0)[^1];
+                    Console.Write(tokenizerStream.Decode(seq));
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("Done!");
+
+            }
+            return 0;
+        }
+    }
+}
+
+
+
+
+
+/*
+  
+ using Microsoft.ML.OnnxRuntimeGenAI;
+
+// path for model and images
+var modelPath = @"d:\phi3\models\Phi-3-vision-128k-instruct-onnx-cpu\cpu-int4-rtn-block-32-acc-level-4";
+
+var foggyDayImagePath = Path.Combine(Directory.GetCurrentDirectory(), "imgs", "foggyday.png");
+var petsMusicImagePath = Path.Combine(Directory.GetCurrentDirectory(), "imgs", "petsmusic.png");
+var img = Images.Load(petsMusicImagePath);
+
+// define prompts
+var systemPrompt = "You are an AI assistant that helps people find information. Answer questions using a direct style. Do not share more information that the requested by the users.";
+string userPrompt = "Describe the image, and return the string 'STOP' at the end.";
+var fullPrompt = $"<|system|>{systemPrompt}<|end|><|user|><|image_1|>{userPrompt}<|end|><|assistant|>";
+
+// load model and create processor
+using Model model = new Model(modelPath);
+using MultiModalProcessor processor = new MultiModalProcessor(model);
+using var tokenizerStream = processor.CreateStream();
+
+// create the input tensor with the prompt and image
+Console.WriteLine("Full Prompt: " + fullPrompt);
+Console.WriteLine("Start processing image and prompt ...");
+var inputTensors = processor.ProcessImages(fullPrompt, img);
+using GeneratorParams generatorParams = new GeneratorParams(model);
+generatorParams.SetSearchOption("max_length", 3072);
+generatorParams.SetInputs(inputTensors);
+
+// generate response
+Console.WriteLine("Generating response ...");
+using var generator = new Generator(model, generatorParams);
+while (!generator.IsDone())
+{
+    generator.ComputeLogits();
+    generator.GenerateNextToken();
+    var seq = generator.GetSequence(0)[^1];
+    Console.Write(tokenizerStream.Decode(seq));
+}
+
+Console.WriteLine("");
+Console.WriteLine("Done!");
+ */
+
+
+/*
+        var filelocationsOption = new Option<string>(
+            name: "locations",
+            description: "File Locations.",
+            isDefault: true,
+            parseArgument: result =>
+            {
+                if (result.Tokens.Count == 0)
+                {
+                    return DEFAULT_MODEL_PATH;
+
+                }
+                string? filePath = result.Tokens.Single().Value;
+                if (!File.Exists(DEFAULT_MODEL_PATH))
+                {
+                    result.ErrorMessage = "DEFAULT_MODEL_PATH file does not exist";
+                    return "";
+                }
+                else
+                {
+                    return DEFAULT_MODEL_PATH;
+                }
+            });
+
+        var usageOption = new Option<string>(
+            name: "usage",
+            description: "Model usage; chat or image.",
+            getDefaultValue: () => "chat");
+
+        var rootCommand = new RootCommand("Phi3 AI App");
+        
+        var locationsCommand = new Command("path", "Path to model files.")
+                            {
+                            filelocationsOption
+                            };
+
+        locationsCommand.SetHandler(async (locations) =>
+        {
+            await RunApp(locations, null);
+        },
+        filelocationsOption);
+
+        rootCommand.AddCommand(locationsCommand);
+
+
+      
+        
+        //var usage_Command = new Command("usage", "Usage: write 'chat' or 'image'.")
+        //                    {
+        //                    usageOption
+        //                    };
+        //locationsCommand.AddCommand(usage_Command);
+        
+
+
+
+        locationsCommand.SetHandler(async (file, usage) =>
+        {
+            await RunApp(file!, usage);
+        },
+        filelocationsOption, usageOption);
+        
+        
+
+        return await rootCommand.InvokeAsync(args);
+        */
---
+++ Utils.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phi3
+{
+    public enum MODELUSE : ushort
+    {
+        CHAT,
+        IMAGE
+    }
+
+    
+
+    public interface IFileLocations
+    {
+        public string ModelDirectory
+        { get; set; }
+
+        public string ModelName
+        { get; set; }
+
+
+    }
+
+    public record FileLocations //: IFileLocations
+    {
+        private readonly string rootdirectory;
+
+        // public required string ModelDirectory
+        public FileLocations()
+        {
+
+        }
+        public FileLocations(string ModelDirectory)
+        {
+            this.ModelDirectory = ModelDirectory;
+        }
+        public FileLocations(string ModelDirectory, string ModelName)
+        {
+            this.ModelDirectory = ModelDirectory;
+            this.ModelName = ModelName;
+        }
+        public string ModelDirectory
+        {
+            get; set;
+            //get => rootdirectory;
+            //init => rootdirectory = @"c:\tmp"
+            //set;
+        }
+
+        public string ModelName
+        { get; set; }
+        public string ModelUsage
+        { get; set; }
+
+        public string Image1_Dir
+        { get; set; }
+
+        public string Image2_Dir
+        { get; set; }
+
+        public override string ToString()
+        {
+            var str = "ModelDirectory = " + (this.ModelDirectory ?? "");
+            str += Environment.NewLine + "ModelName = " + (this.ModelName ?? "");
+            str += Environment.NewLine + "ModelUsage = " + (this.ModelUsage ?? "");
+            return str;
+        }
+    }
+}
